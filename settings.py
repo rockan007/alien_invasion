2a228f96e8a7b714c8bb3007e2ddeb8958622118
rockan007 <rockan007@gmail.com>
2021-10-13 13:56:46 +0800

    新建项目

---
+++ settings.py
@@ -0,0 +1,16 @@
+'''
+Description: 
+Author: Andy An
+Date: 2021-10-12 18:05:28
+LastEditors: Andy An
+LastEditTime: 2021-10-12 18:07:49
+FilePath: \alien-invasion\setting.py
+'''
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    def __init__(self):
+        """Initialize the game settings"""
+        # Screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color =(230,230,230)
---
+++ ship.py
@@ -0,0 +1,26 @@
+'''
+Description: 
+Author: Andy An
+Date: 2021-10-12 18:30:48
+LastEditors: Andy An
+LastEditTime: 2021-10-12 18:30:49
+FilePath: \alien-invasion\ship.py
+'''
+import pygame
+
+class Ship: 
+    """A class to manage the ship"""
+
+    def __init__(self,ai_game):
+        """Initialize the ship and set its starting position"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        # Start each new shiip at the bottom of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+    def blitme(self):
+        """Draw the ship at its current location"""
+        self.screen.blit(self.image,self.rect)
